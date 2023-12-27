31b2525ec3ecd58b92cac1ef946ddaa9c5fc0845
vilayat-ali <vilayatcodemysite@gmail.com>
2023-12-28 04:55:37 +0530

    Initial Commit

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,12 @@
 //! MongORM-RS is a fast, simple, and powerful Object-Document Mapper (ODM) for MongoDB in Rust. It provides an ORM-like API to interact with MongoDB databases seamlessly.
 
 pub mod error;
+
+pub use error::Error;
+
+use mongodb::{options::ClientOptions, Client};
+
+pub async fn establish_connection(conn_str: &str) -> crate::Error {
+    let mut client_options = ClientOptions::parse(conn_str).await.unwrap();
+    Error::ConnectionFailed("sad".to_owned())
+}
